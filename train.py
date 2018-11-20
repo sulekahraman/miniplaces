70aa88103d72c50ed19bdaabf57ca0d8548ac491
William Rodriguez <wrod@mit.edu>
2018-11-19 19:35:26 -0500

    Everything default, except switched SGD to Adam

--- train.py
+++ train.py
@@ -143,7 +143,7 @@ def run():
     # changed to Adam
     # lr 
 
-    optimizer = optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
     # make sure to verify overfitting
     epoch = 1

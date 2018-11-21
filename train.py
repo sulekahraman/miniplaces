798893bedddd1b64bd65fafc94bdc9daf565e49c
Ubuntu <ubuntu@ip-172-31-84-149.ec2.internal>
2018-11-21 04:00:21 +0000

    exp2 done

--- train.py
+++ train.py
@@ -163,7 +163,7 @@ def run():
 
     # can input a weight decay argument here, shouldn't be very large since we have a large dataset , try (1e-3)
     # also try to change the learning rate  
-    optimizer = optim.SGD(model.parameters(), lr=.1,weight_decay =5e-4)  # weight_decay=0 , since adam is faster, might be better for lower epochs 
+    optimizer = optim.SGD(model.parameters(), lr=1e-4,weight_decay =0)  # weight_decay=0 , since adam is faster, might be better for lower epochs 
     # scheduler = optim.lr_scheduler.LambdaLR(optimizer,lambda x:0.1*x)
     #scheduler takes optimizer as arguemnt, scheduler.step()
     #simple multistep scheduler , 150 epochs, drop lr at 50, and 100,multiply lr by 0.1 , increase learning rate to something like 0.1
@@ -202,7 +202,7 @@ def run():
         val_t5[epoch] = 100 - val_top5
 
         # save after every epoch
-        torch.save(model.state_dict(), "models/SGD_resnet_18_lr_.1_weight_decay_5e-4.%d" % epoch)
+        torch.save(model.state_dict(), "models/exp2.%d" % epoch)
 
         # TODO: Calculate classification error and Top-5 Error
         # on training and validation datasets here
@@ -213,9 +213,9 @@ def run():
 
     # with open('output/dropout/train_top1.json', 'w') as out1:
     #     json.dump(train_t1, out1)
-    with open('output/SGD_resnet_18_lr_.1_weight_decay_5e-4/train_top5.json', 'w') as out2:
+    with open('output/exp2/train_top5.json', 'w') as out2:
         json.dump(train_t5, out2)
-    with open('output/SGD_resnet_18_lr_.1_weight_decay_5e-4/val_top5.json', 'w') as out3:
+    with open('output/exp2/val_top5.json', 'w') as out3:
         json.dump(val_t5, out3)
     # with open('output/dropout/val_top1.json', 'w') as out4:
     #     json.dump(val_t1, out4)

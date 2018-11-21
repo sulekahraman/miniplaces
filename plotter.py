cc242c6ac1d5da6533ac55ce6c81e4feb096b98d
Sule Kahraman <sulek@mit.edu>
2018-11-20 22:41:25 -0500

    Deleted models from git

--- plotter.py
+++ plotter.py
@@ -10,10 +10,10 @@ with open("output/default/train_top5.json","r") as t5:
     train5 = json.load(t5)
 with open("output/default/val_top5.json","r") as v5:
     val5 = json.load(v5)
-with open("output/exp5/val_top5.json","r") as v:
-    mod_train5 = json.load(v)
-with open("output/exp5/train_top5.json","r") as t:
-    mod_val5 = json.load(t)
+with open("output/exp1/val_top5.json","r") as v:
+    mod_val5 = json.load(v)
+with open("output/exp1/train_top5.json","r") as t:
+    mod_train5 = json.load(t)
 
 # train_top1 = [value for (key, value) in sorted(train1.items())]
 train_top5 = [value for (key, value) in sorted(train5.items(), key=lambda x: int(x[0]))]
@@ -23,16 +23,16 @@ mod_train_top5 = [value for (key, value) in sorted(mod_train5.items(), key=lambd
 val_top5 = [value for (key, value) in sorted(val5.items(), key=lambda x: int(x[0]))]
 mod_val_top5 = [value for (key, value) in sorted(mod_val5.items(), key=lambda x: int(x[0]))]
 
-print(train_top5)
-x = range(1, len(train_top5)+1)
+# print(mod_train_top5)
+x = range(1, len(mod_train_top5)+1)
 # plt.plot(x, train_top1, label = "Top-1 Training Error")  
 # plt.plot(x, val_top1, label = "Top-1 Validation Error")
 plt.plot(x, train_top5, label = "Training Err of Original")
 plt.plot(x, val_top5, label  = "Validation Err of Original")
-plt.plot(x, mod_train_top5[:10], label = "Training Err of Modified")
-plt.plot(x, mod_val_top5[:10], label  = "Validation Err of Modified")
+plt.plot(x, mod_train_top5, label = "Training Err of Modified")
+plt.plot(x, mod_val_top5, label  = "Validation Err of Modified")
 plt.xlabel("epoch")
 plt.ylabel("Top-5 Error (%)")
-plt.title("SGD Optimizer original:lr=1e-3, modified lr=0.1, with scheduler, and weight_decay=5e-4")
+plt.title("SGD Optimizer original:lr=1e-3, modified lr=0.1")
 plt.legend()
 plt.show()
